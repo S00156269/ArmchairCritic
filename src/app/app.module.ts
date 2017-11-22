b26cf1a2e4ca099009382b0145009ce782c9c5e8
Ciara <32096395+ciaralynch@users.noreply.github.com>
2017-11-22 13:15:51 +0000

    Added search, reviewers, profile components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FilmsComponentComponent } from './films-component/films-component.component';
 import { ListComponentComponent } from './list-component/list-component.component';
+import { SearchComponent } from './search/search.component';
+import { ReviewersComponent } from './reviewers/reviewers.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FilmsComponentComponent,
-    ListComponentComponent
+    ListComponentComponent,
+    SearchComponent,
+    ReviewersComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
