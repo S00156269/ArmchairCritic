521499d84c7e938d6a01d088a6f2ba444f65031b
S00146154 <s00146154@mail.itsligo.ie>
2017-11-27 22:59:12 +0000

    Skeleton
    Added HTML side bar and fleshed out some components for data entry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {Http} from '@angular/http';
 
 
 import { AppComponent } from './app.component';
@@ -27,27 +26,4 @@ import { ProfileComponent } from './profile/profile.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { 
-
-  constructor(/* private http:Http */) {}
-
-  searchMovie(MovieTitle:string) {
-    console.log(MovieTitle);
-    // this.http.get("https://api.themoviedb.org/3/movie/popular?page=1&language=en-US&api_key=b1486a6362ec4507649074230d7aa50b")
-    // You can create a javascript file and call it from here
-
-    var data = "{}";
-
-    var xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-
-    xhr.addEventListener("readystatechange", function () {
-      if (this.readyState === this.DONE) {
-        console.log(this.responseText);
-      }
-    });
-
-    xhr.open("GET", "https://api.themoviedb.org/3/movie/2/images?language=en-US&api_key=b1486a6362ec4507649074230d7aa50b");
-
-    xhr.send(data);
-  }
 }
